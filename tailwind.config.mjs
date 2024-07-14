3fb98f99656ae647d73ae8c7492663e97586c860
KayeeGuo <kylelee07@gmail.com>
2024-07-13 21:13:20 -0400

    pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				content: {
+					1: "hsl(var(--color-content1) / <alpha-value>)",
+					2: "hsl(var(--color-content2) / <alpha-value>)",
+				},
+				accent: {
+					1: "hsl(var(--color-accent1) / <alpha-value>)",
+					2: "hsl(var(--color-accent2) / <alpha-value>)",
+					3: "hsl(var(--color-accent2) / <alpha-value>)",
+				},
+				bg: "hsl(var(--color-bg) / <alpha-value>)",
+				highlight: "hsl(var(--color-highlight) / <alpha-value>)",
+			}
+		},
 	},
 	plugins: [],
 }
