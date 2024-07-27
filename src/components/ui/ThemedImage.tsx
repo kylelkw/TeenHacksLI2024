56f1948b5bbf16530146748dc22a897eb746b416
Clayton Zickerman <cczickerman@gmail.com>
2024-07-26 20:47:30 -0400

    fix bug with themed image making it so there could only be one per page

---
+++ src/components/ui/ThemedImage.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef } from "react";
+
+interface Props {
+    darkModeUrl: string;
+    lightModeUrl: string;
+    className?: string;
+    loading?: "eager" | "lazy";
+    alt?: string;
+}
+
+export default function ThemedImage({ darkModeUrl, lightModeUrl, alt, className, loading = "lazy" }: Props) {
+    const ref = useRef(null);
+    useEffect(() => {
+        const updateImage = () => {
+            const theme = document.documentElement.dataset.theme;
+            const src = theme === "dark" ? darkModeUrl : lightModeUrl;
+            ref.current.src = src;
+        };
+
+        const observer = new MutationObserver((mutations) => {
+            if (mutations.filter((m) => m.attributeName === "data-theme").length > 0) {
+                updateImage();
+            }
+        });
+        observer.observe(document.documentElement, { attributes: true });
+
+        updateImage();
+    });
+    return <img className={className} id="img" loading={loading} alt={alt} ref={ref} />;
+}
