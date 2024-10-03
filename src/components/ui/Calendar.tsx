55289774b1cd14661ad43bdf32d289e0e2866df6
Clayton Zickerman <cczickerman@gmail.com>
2024-10-03 11:03:32 -0400

    finish parts of homepage

--- src/components/ui/Calendar.tsx
+++ src/components/ui/Calendar.tsx
@@ -1,7 +1,7 @@
 export default function Calendar() {
     return (
-        <div className="w-full bg-content-1 h-full rounded-3xl flex flex-col items-center relative blur-md z-0">
-            <h1 className="text-display text-7xl text-bg font-medium mt-10">November 2024</h1>
+        <div className="w-full bg-content-1 h-full rounded-3xl flex flex-col items-center relative blur-md z-0 select-none">
+            <h1 className="text-display text-7xl text-bg font-medium mt-10">Janurary 2024</h1>
             <div className="mt-14 grid-cols-7 grid gap-1 bg-content-3 border-4 border-bg">
                 {Array.from({ length: 5 * 7 }).map((num, i) => {
                     return <div className="w-20 h-20 bg-content-1" />;
